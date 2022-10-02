4d71cb9caa6b31a143471f34ec218f0dd5f366b8
Daniel Loomer <fuzetsu@users.noreply.github.com>
2022-10-02 19:28:09 -0400

    find/replace words that mess up tts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -10,13 +10,26 @@ const fetchDoc = (url: string) =>
     .then(res => (res.ok ? res.text() : Promise.reject()))
     .then(html => new DOMParser().parseFromString(html, 'text/html'))
 
+// these words by themselves in a paragraph break ios speak screen
+const TTS_BREAK_FILTER: [RegExp, string][] = [
+  ['hu', 'who'],
+  ['pue', 'poo'],
+  ['sou', 'so']
+].map(([word, rep]) => [new RegExp(`\\b${word}\\b`, 'gi'), rep])
+
 const chapterCache = new Map<string, string[]>()
+
 export const fetchChapter = async (novelId: string, chapter: number) => {
   const cacheKey = novelId + chapter
   const cachedChapter = chapterCache.get(cacheKey)
   if (cachedChapter) return cachedChapter
   const doc = await fetchDoc(`https://novelfull.com/${novelId}/chapter-${chapter}.html`)
-  const lines = qq('#chapter-content p', doc).map(paragraph => paragraph.textContent || '')
+  const lines = qq('#chapter-content p', doc).map(paragraph =>
+    TTS_BREAK_FILTER.reduce(
+      (acc, [regex, rep]) => acc.replace(regex, rep),
+      paragraph.textContent ?? ''
+    )
+  )
   chapterCache.set(cacheKey, lines)
   return lines
 }
