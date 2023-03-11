3660369f1dc57062cac0a6ca2c9ef53278ada56c
Daniel Loomer <fuzetsu@users.noreply.github.com>
2023-03-11 18:53:39 -0500

    add action menu to scroll control - added action to go home

--- src/cmp/ScrollControl.tsx
+++ src/cmp/ScrollControl.tsx
@@ -1,10 +1,16 @@
 import { useRef, useState } from 'preact/hooks'
 import { useScroll } from '../lib/hooks'
-import { qq, scrollToBottom, scrollToTop } from '../lib/util'
+import { classNames, qq, scrollToBottom, scrollToTop } from '../lib/util'
+
+type Link = { label: string } & ({ url: string } | { onClick(): void })
+
+const extraLinks: Link[] = [{ label: 'Home', url: '/' }]
 
 export function ScrollControl() {
   const [scrollingDown, setScrollingDown] = useState(true)
   const [scrollPercentage, setScrollPercentage] = useState('0')
+
+  const [showExtra, setShowExtra] = useState(false)
 
   const lastScroll = useRef(0)
   useScroll(() => {
@@ -39,8 +45,37 @@ export function ScrollControl() {
   }
 
   return (
-    <button aria-hidden className="scroll-control" onClick={scrollToNextChapter}>
-      {scrollingDown ? '\u25bc' : '\u25b2'} {scrollPercentage}
-    </button>
+    <div aria-hidden className="scroll-control">
+      <div
+        className={classNames(
+          'scroll-control__extra',
+          showExtra && 'scroll-control__extra--visible'
+        )}
+      >
+        {extraLinks.map(link => (
+          <button
+            className="scroll-control__button"
+            onClick={() => {
+              if ('url' in link) location.hash = link.url
+              else link.onClick()
+            }}
+          >
+            {link.label}
+          </button>
+        ))}
+      </div>
+      <button
+        className={classNames(
+          'scroll-control__show-extra',
+          showExtra && 'scroll-control__show-extra--upside-down'
+        )}
+        onClick={() => setShowExtra(!showExtra)}
+      >
+        {'\u2303'}
+      </button>
+      <button className="scroll-control__button" onClick={scrollToNextChapter}>
+        {scrollingDown ? '\u25bc' : '\u25b2'} {scrollPercentage}
+      </button>
+    </div>
   )
 }
